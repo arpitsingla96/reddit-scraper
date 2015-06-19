b45f277cc80a398d44f73d554bfe0191419c455c
arpit <arpit270896@gmail.com>
2015-06-19 15:53:26 +0530

    modified reddit-scraper to write all the urls only to subreddit_urls folder

--- reddit-scraper.py
+++ reddit-scraper.py
@@ -5,39 +5,41 @@ import httplib
 import time
 from bs4 import BeautifulSoup
 from separator import filter_output, separate
+import os
 
 URL = "http://www.reddit.com/r/"
 HEADER = { 'User-Agent' : 'hubble-tags' }
 CONNECTION = httplib.HTTPConnection('www.reddit.com')
+dirname = 'subreddit_urls/'
 
 REDDIT_CATEGORIES = [
-    ('programming', 'machinelearning', 150),
-    ('programming', 'compsci', 250),
-    ('programming', 'programming', 150),
+	('programming', 'machinelearning', 150),
+	('programming', 'compsci', 250),
+	('programming', 'programming', 150),
 
-    ('business', 'business', 200),
+	('business', 'business', 200),
 
-    ('design', 'web_design', 300),
-    ('design', 'graphic_design', 300),
+	('design', 'web_design', 300),
+	('design', 'graphic_design', 300),
 
-    ('entertaiment', 'Music', 150),
-    ('entertaiment', 'movies', 150),
-    ('entertaiment', 'books', 150),
-    ('entertaiment', 'television', 150),
+	('entertaiment', 'Music', 150),
+	('entertaiment', 'movies', 150),
+	('entertaiment', 'books', 150),
+	('entertaiment', 'television', 150),
 
-    ('science', 'science', 100),
-    ('science', 'Physics', 100),
-    ('science', 'chemistry', 100),
-    ('science', 'biology', 100),
-    ('science', 'math', 100),
+	('science', 'science', 100),
+	('science', 'Physics', 100),
+	('science', 'chemistry', 100),
+	('science', 'biology', 100),
+	('science', 'math', 100),
 
-    ('security', 'networking', 200),
-    ('security', 'hacking', 200),
-    ('security', 'ComputerSecurity', 200),
+	('security', 'networking', 200),
+	('security', 'hacking', 200),
+	('security', 'ComputerSecurity', 200),
 
-    ('worldnews', 'worldnews', 200),
-    ('worldnews', 'news', 200),
-    ('worldnews', 'truenews', 200),
+	('worldnews', 'worldnews', 200),
+	('worldnews', 'news', 200),
+	('worldnews', 'truenews', 200),
 	]
 
 
@@ -56,16 +58,19 @@ def get_next_link(soup) :
 	return next_link
 
 
-def print_all_links(reddit, subreddit, soup, size) :
+def output_url(output_file, soup, size) :
 	post_a_tags = soup.find_all('a', class_='title may-blank ')
 	for post_a_tag in post_a_tags :
 		if post_a_tag['href'][:4] == 'http' :
-			print(reddit, subreddit, post_a_tag['href'], post_a_tag.string.encode('ascii','ignore'))
+			output_file.write(post_a_tag['href'])
 			size+=1
 	return size
 
 def main() :
+	if os.path.exists(dirname) == False :
+		os.makedirs(dirname)
 	for reddit, subreddit, limit in REDDIT_CATEGORIES :
+		output_file = open(dirname + subreddit, 'w')
 		url = URL + subreddit
 		print(url)
 		size = 0
@@ -75,7 +80,8 @@ def main() :
 			next_link = get_next_link(soup)
 			if next_link == "" : break
 			partial_url = next_link.split('/')[-1]
-			size = print_all_links(reddit, subreddit, soup, size)
+			size = output_url(output_file, soup, size)
+		output_file.close()
 	CONNECTION.close()
 
 if __name__ == "__main__" : main()
